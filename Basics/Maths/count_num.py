8788ed33544ea4ec23c37cc4c137eaffa0d9811b
Aakash Chari <legendarypowers07@gmail.com>
2026-05-12 12:37:55 +0000

    started off with math

---
+++ Basics/Maths/count_num.py
@@ -0,0 +1,11 @@
+n = 12345
+e = str(n)
+f = len(e)
+
+cnt = 0
+
+for i in range(f):
+    cnt += 1
+
+
+print(cnt)
---
+++ Basics/Maths/reverse_a_num.py
@@ -0,0 +1,20 @@
+#  we need to reverse a number
+# 1234 -> 4321
+
+n = 35
+
+a = list(str(n))
+a.reverse()
+print(''.join(a))
+
+
+# another way without using library functions
+
+n = 1234
+rev = 0
+while n > 0:
+    rem = n % 10
+    rev = rev * 10 + rem
+    n = n // 10
+
+print(rev)
