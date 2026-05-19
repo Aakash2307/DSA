cecaea12182402df2409df8e1c4165e0cc4c0006
Aakash Chari <legendarypowers07@gmail.com>
2026-05-19 13:27:44 +0000

    added 2 hashing probs

---
+++ Basics/Recursion/count_freq.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def count_frea(self, nums ):
+        freq = {}
+
+        for num in nums:
+            freq[num] = freq.get(num ,0) +1
+
+        result = []
+
+        for key , value in freq.items():
+            result.append((key , value))
+
+
+        return result
+
+    
+solution = Solution()
+y = solution.count_frea([1,2,3,4,5,6,7])
+
+print(y)
