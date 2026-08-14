2c0428fdadd170a7a85583e81b76299a50e7f9c2
Aakash Chari <legendarypowers07@gmail.com>
2026-08-14 16:48:55 +0530

    majority_element

---
+++ Arrays/169_Majority_Element.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def majorityElement(self, nums):
+
+        n = len(nums)
+        nums.sort()
+        count = 0 
+
+
+        for i in range(1,n):
+
+            if nums[i] == nums[i-1]:
+                count += 1
+            else :
+                count =1 
+
+            
+            if  count > n//2:
+                return nums[i]
+
+            
+        return nums[0]
+
+        
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
