af115ecfa6ce89312e2af2dbddccfdc72c225654
Aakash Chari <legendarypowers07@gmail.com>
2026-08-14 15:21:07 +0530

    pascal

---
+++ Arrays/118_Pascal_triangle.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def generate(self, numRows):
+
+        #  lets create a list first 
+        ans = []
+
+        # now we need to loop in the numRows 
+        for i in range(numRows):
+
+            #  we saw that the rows increments once from the prev row so 
+            row = [1] * (i+1)
+
+            # we know that the increments happens in the middle 
+            #  the row start and end are same so we leave that 
+            # and take look at the middle one only so we run a loop there too 
+
+            for j in range(1,i):
+                # we say here that the num is actually the sum of the ith element and i - 1 element 
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+
+            # now append that vals into that row 
+
+            ans.append(row)
+
+
+        return ans 
+
+        
+
